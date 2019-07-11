2028714b5034c3ddbee139f5912a4542b096ecd5
jianghai0702 <37363753+jianghai0702@users.noreply.github.com>
2019-07-11 23:48:59 +0800

    init project

---
+++ src/main/scala/com/Spark_DW/DW2DM.scala
@@ -0,0 +1,18 @@
+package Spark_DW
+
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.hive.HiveContext
+
+object DW2DM {
+  def main(args: Array[String]): Unit = {
+
+    System.setProperty("hadoop.home.dir", "")
+    System.setProperty("HADOOP_USER_NAME", "root")
+    val spark = SparkSession.builder().appName("DW2DM").master("local[2]").getOrCreate()
+    val hiveContext = new HiveContext(spark.sparkContext)
+
+
+    val result: DataFrame = hiveContext.sql("sql")
+
+  }
+}
