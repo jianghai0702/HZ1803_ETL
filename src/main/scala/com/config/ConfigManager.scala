fe04b57d4ad55064bc8aa36e8764a3d31d9a68dc
jianghai0702 <37363753+jianghai0702@users.noreply.github.com>
2019-07-15 17:58:14 +0800

    fix

--- src/main/scala/com/config/ConfigManager.scala
+++ src/main/scala/com/config/ConfigManager.scala
@@ -7,14 +7,14 @@ object ConfigManager {
   private val prop = new Properties()
   // 通过类加载器方法来加载指定的配置文件
   try{
-    val in_dws = ConfigManager.getClass.getClassLoader.getResourceAsStream("dwd_dws.properties")
-
-    val dw2dm = ConfigManager.getClass.getClassLoader.getResourceAsStream("dw2dm.properties")
-
+    val dws_user_month = ConfigManager.getClass.getClassLoader.getResourceAsStream("dws_user_month.properties")
+    val dm_user_basic = ConfigManager.getClass.getClassLoader.getResourceAsStream("dm_user_basic.properties")
+    val dm_user_visit = ConfigManager.getClass.getClassLoader.getResourceAsStream("dm_user_visit.properties")
     val in_basic = ConfigManager.getClass.getClassLoader.getResourceAsStream("basic.properties")
 
-    prop.load(in_dws)
-    prop.load(dw2dm)
+    prop.load(dws_user_month)
+    prop.load(dm_user_basic)
+    prop.load(dm_user_visit)
     prop.load(in_basic)
   }catch {
     case e:Exception=>e.printStackTrace()
